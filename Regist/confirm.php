1f374081cef1e00c477c0f7f0ed9875961235b5b
otgon123 <220054eo@yse-c.net>
2023-10-13 12:07:11 +0900

    update

--- Regist/confirm.php
+++ Regist/confirm.php
@@ -1,34 +1,40 @@
 <?php
-//php program
-$Regist = $_POST 
+// PHP program
+$regist = $_POST;
+// var_dump($regist);
+$genders['male'] = "男性"; 
+$genders['female'] = "女性"; 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=3.0">
     <title>Regist</title>
 </head>
-
 <body>
     <h1>確認画面</h1>
-    <p>okay?</p>
+    <p>この内容で登録してもよろしいですか？</p>
     <form action="" method="post">
         <div>
             <label for="">氏名</label>
-            <?= $Regist['name']?>
+            <?= $regist['name'] ?>
         </div>
         <div>
             <label for="">Email</label>
-            <?= $Regist['email']?>
+            <?= $regist['email'] ?>
         </div>
         <div>
-            <label for="">パスワード</label>
+            <label for="">生年月日</label>
+            <?= $regist['year'] ?>/<?= $regist['month'] ?>/<?= $regist['day'] ?>
+        </div>
+        <div>
+            <label for="">性別</label>
+            <?= $genders[$regist['gender']] ?>
         </div>
         <button>登録</button>
     </form>
 </body>
-
 </html>
