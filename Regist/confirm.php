1e5af32fdda675d75c3ec64f4504038c29d0de15
otgon123 <220054eo@yse-c.net>
2023-10-20 11:52:02 +0900

    update

--- Regist/confirm.php
+++ Regist/confirm.php
@@ -6,14 +6,14 @@ session_regenerate_id(true);
 // POSTリクエストのデータを 変数 regist に代入
 $regist = check($_POST);
 
-//TODO: データチェック
-// パスワードが8文字以上、20文字以内でなければ、入力画面にリダイレクト
-if (
-    mb_strlen($regist['password']) < 8 ||
-    mb_strlen($regist['password']) > 20
-) {
-    $_SESSION['errors']['password'] = "パスワードは８文字以上、20文字以内で入力してください";
+//TODO: データチェック（バリデーション）
+unset($_SESSION['errors']);
+validateName($regist['name']);
+validatePassword($regist['password']);
+
+if (isset($_SESSION['errors'])) {
     header('Location: input.php');
+    exit;
 }
 
 // セッション保存
@@ -22,6 +22,36 @@ $_SESSION['regist'] = $regist;
 $genders['male'] = "男性";
 $genders['female'] = "女性";
 
+/**
+ * バリデート（validate）
+ */
+function validateName($name)
+{
+    $error = "";
+    // パスワードが8文字以上、20文字以内でなければ、入力画面にリダイレクト
+    if (empty($name)) {
+        $error = "名前を入力してください";
+    }
+    if ($error) $_SESSION['errors']['name'] = $error;
+}
+
+/**
+ * バリデート（validate）
+ */
+function validatePassword($password)
+{
+    $error = "";
+    // パスワードが8文字以上、20文字以内でなければ、入力画面にリダイレクト
+    if (empty($password)) {
+        $error = "パスワードは８文字以上、20文字以内で入力してください";
+    } else if (
+        mb_strlen($password) < 8 ||
+        mb_strlen($password) > 20
+    ) {
+        $error = "パスワードは８文字以上、20文字以内で入力してください";
+    }
+    if ($error) $_SESSION['errors']['password'] = $error;
+}
 
 /**
  * サニタイズ
